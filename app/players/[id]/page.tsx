cf2ddf6c0f681bd79419da32d6a10088eff491ee
Eric George <ericgeorge@Shyjis-MacBook-Air.local>
2023-09-15 23:49:46 -0400

    Added player page

---
+++ app/players/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+export default function PlayerInfo({ params }: any) {
+  const { id } = params;
+  // console.log(id);
+  return (
+    <>
+      <img
+        src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${id}.png`}
+        alt="Photo for player"
+        width={220}
+        height={280}
+      />
+    </>
+  );
+}
